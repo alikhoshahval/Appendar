7ba3647167e81a00be07dc4eb67cdab35f471405
Mossi <mail4mosi@gmail.com>
2021-09-12 14:05:15 +0430

    registeration function

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, Renderer2 } from '@angular/core';
 import { Router }    from '@angular/router';
-import { NgForm }    from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm }    from '@angular/forms';
 import pageSettings from './../config/page-settings';
 
 @Component({
@@ -10,7 +11,13 @@ import pageSettings from './../config/page-settings';
 })
 export class RegisterComponent implements OnDestroy {
   pageSettings = pageSettings;
-  constructor(private router: Router, private renderer: Renderer2) {
+  form:FormGroup;
+
+  constructor(private router: Router,
+    private renderer: Renderer2,
+    private formBuilder:FormBuilder,
+    private http: HttpClient
+    ) {
     this.pageSettings.pageEmpty = true;
     this.renderer.addClass(document.body, 'bg-white');
   }
@@ -20,12 +27,27 @@ export class RegisterComponent implements OnDestroy {
     this.renderer.removeClass(document.body, 'bg-white');
   }
 
-  formSubmit(f: NgForm) {
-    this.router.navigate(['dashboard/v3']);
-  }
+  // formSubmit(f: NgForm) {
+  //   this.router.navigate(['dashboard/v3']);
+  // }
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name:'',
+      email:'',
+      password:''
+    });
   }
 
+  submit():void{
+    //call backend to register the user
+  //  console.log(this.form.getRawValue());
+   this.http.post('http://localhost:8000/api/register',this.form.getRawValue() )
+  //  .subscribe(res => {
+  //   // console.log(res);
+  //   this.router.navigate(['/login'])
+  //  });
+      .subscribe(()=> this.router.navigate(['/login']));
+  }
 }
 
 
